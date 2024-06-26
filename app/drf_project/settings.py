a07783daa4f05620fa75416d17e67edf46e38d58
Jumar <jumarvermeulen99@gmail.com>
2024-06-26 17:54:30 +0200

    Djnago Deployment Checklist

--- app/drf_project/settings.py
+++ app/drf_project/settings.py
@@ -18,6 +18,18 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
+if not DEBUG:
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    X_FRAME_OPTIONS = "DENY"
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+    SECURE_REFERRER_POLICY = "same-origin"
 
 # Application definition
 
@@ -122,11 +134,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "movies.CustomUser"
 
-# Disable the Browsable API in production.
-if not DEBUG:
-    REST_FRAMEWORK = {
-        "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",)
-    }
+# # Disable the Browsable API in production.
+# if not DEBUG:
+#     REST_FRAMEWORK = {
+#         "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",)
+#     }
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # WhiteNoise part 2: Deployment
 
