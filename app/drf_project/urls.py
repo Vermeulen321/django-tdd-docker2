30cc876c62718cca531773d12dbe8ccf1ab5e8ab
Jumar <jumarvermeulen99@gmail.com>
2024-06-26 10:35:12 +0200

    Part1 finished Finished the RESTful Routes part, moving to part 2 of tut

--- app/drf_project/urls.py
+++ app/drf_project/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from .views import ping
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('ping/', ping, name="ping"),
+    path("admin/", admin.site.urls),
+    path("ping/", ping, name="ping"),
+    path("", include("movies.urls")),
 ]
